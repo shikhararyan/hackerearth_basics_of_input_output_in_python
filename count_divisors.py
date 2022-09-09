837e8c3718cba3bbe185f70760e979b341331ab5
shikhar aryan <shikhararyan0@gmail.com>
2022-09-09 08:27:21 +0530

    Update count_divisors.py

--- count_divisors.py
+++ count_divisors.py
@@ -5,3 +5,20 @@ for i in range(int(lis[0]),int(lis[1])+1):
     if  i % int(lis[2]) == 0:
         k= k+1
 print(k)
+
+
+
+
+
+
+
+
+#another approach is by splitting the input into three seperate variables 
+l,r,k= (input().split())                 # Reading input from STDIN
+count = 0
+for i in range(int(l),int(r)+1):
+        if i % int(k) == 0:
+                count = count +1
+        else:
+                pass
+print(count)
